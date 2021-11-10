9cccd8bce42410b02d81ef8515b5ba2e5b4fe558
freddie <chakangost@gmail.com>
2021-11-10 10:44:17 +0900

    layout code lab first commit

--- week 2-1-Layouts in JetpackCompose/ComposeLayoutCodelab/app/src/main/java/kr/co/freddie/composelayoutcodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/ComposeLayoutCodelab/app/src/main/java/kr/co/freddie/composelayoutcodelab/MainActivity.kt
@@ -3,10 +3,11 @@ package kr.co.freddie.composelayoutcodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import kr.co.freddie.composelayoutcodelab.ui.theme.ComposeLayoutCodelabTheme
 
@@ -15,9 +16,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLayoutCodelabTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    PhotographerCard()
                 }
             }
         }
@@ -25,14 +25,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun PhotographerCard() {
+    Column {
+        Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text("3 minutes ago", style = MaterialTheme.typography.body2)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
+fun PhotographerCardPreview() {
     ComposeLayoutCodelabTheme {
-        Greeting("Android")
+        PhotographerCard()
     }
 }
