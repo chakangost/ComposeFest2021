cf83569bb2b37399382425348e539d8bbe76229c
freddie <chakangost@gmail.com>
2021-11-15 14:51:12 +0900

    completed Complex custom layout

--- week 2-1-Layouts in JetpackCompose/ComposeLayoutCodelab/app/src/main/java/kr/co/freddie/composelayoutcodelab/CustomLayoutActivity.kt
+++ week 2-1-Layouts in JetpackCompose/ComposeLayoutCodelab/app/src/main/java/kr/co/freddie/composelayoutcodelab/CustomLayoutActivity.kt
@@ -3,10 +3,19 @@ package kr.co.freddie.composelayoutcodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.AlignmentLine
 import androidx.compose.ui.layout.FirstBaseline
 import androidx.compose.ui.layout.Layout
@@ -22,33 +31,82 @@ class CustomLayoutActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLayoutCodelabTheme {
-                TextWithPaddingToBaselinePreview()
-
-                TextWithNormalPaddingPreview()
+//                TextWithPaddingToBaselinePreview()
+//                TextWithNormalPaddingPreview()
+//                ChipPreview()
+                LayoutsCodelabPreview()
             }
         }
     }
 
     @Composable
-    fun CustomLayout(
+    fun StaggeredGrid(
         modifier: Modifier = Modifier,
+        rows: Int = 3,
         content: @Composable () -> Unit
     ) {
         Layout(
             modifier = modifier,
             content = content
         ) { measurables, constraints ->
-            val placeables = measurables.map { measurable ->
-                measurable.measure(constraints)
+            val rowWidths = IntArray(rows) { 0 }
+            val rowHeights = IntArray(rows) { 0 }
+
+            val placeables = measurables.mapIndexed { index, measurable ->
+
+                val placeable = measurable.measure(constraints)
+
+                val row = index % rows
+                rowWidths[row] += placeable.width
+                rowHeights[row] = rowHeights[row].coerceAtLeast(placeable.height)
+
+                placeable
             }
 
-            var yPosition = 0
+            val width = rowWidths.maxOrNull()
+                ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
 
-            layout(constraints.maxWidth, constraints.maxHeight) {
-                placeables.forEach { placeable ->
-                    placeable.placeRelative(x = 0, y = yPosition)
-                    yPosition += placeable.height
+            val height = rowHeights.sumOf { it }
+                .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+            val rowY = IntArray(rows) { 0 }
+            for (i in 1 until rows) {
+                rowY[i] = rowY[i-1] + rowHeights[i-1]
+            }
+
+            layout(width, height) {
+                val rowX = IntArray(rows) { 0 }
+
+                placeables.forEachIndexed { index, placeable ->
+                    val row = index % rows
+                    placeable.placeRelative(
+                        x = rowX[row],
+                        y = rowY[row]
+                    )
+                    rowX[row] += placeable.width
                 }
+            }
+        }
+    }
+
+    @Composable
+    fun Chip(modifier: Modifier = Modifier, text: String) {
+        Card(
+            modifier = modifier,
+            border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Row(
+                modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(16.dp, 16.dp)
+                        .background(color = MaterialTheme.colors.secondary)
+                )
+                Spacer(Modifier.width(4.dp))
+                Text(text = text)
             }
         }
     }
@@ -71,6 +129,40 @@ class CustomLayoutActivity : ComponentActivity() {
             }
         }
     )
+
+    private val topics = listOf(
+        "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+        "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+        "Religion", "Social sciences", "Technology", "TV", "Writing"
+    )
+
+
+    @Composable
+    fun BodyContent(modifier: Modifier = Modifier) {
+        Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
+            StaggeredGrid(modifier = modifier, rows = 5) {
+                for (topic in topics) {
+                    Chip(modifier = Modifier.padding(8.dp), text = topic)
+                }
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun LayoutsCodelabPreview() {
+        ComposeLayoutCodelabTheme {
+            BodyContent()
+        }
+    }
+
+    @Preview
+    @Composable
+    fun ChipPreview() {
+        ComposeLayoutCodelabTheme {
+            Chip(text = "Hi there")
+        }
+    }
 
     @Preview
     @Composable
